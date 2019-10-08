c49d2295e9fe52bf22fd0a2c3fcce65cf3c5ea99
Ry Koch <rythehuman@gmail.com>
2019-10-08 08:03:42 -0700

    add config for disabling and return more useful info

--- lib/lambda_git.ex
+++ lib/lambda_git.ex
@@ -9,29 +9,46 @@ defmodule LambdaGit do
   ## Examples
 
     iex> LambdaGit.init()
-    {:ok, "/tmp/git"}
+    {:ok, %{template_dir: "/tmp/git/usr/share/git-core/template", exec_path: "/tmp/git/usr/libexec/git-core", ld_library_path: "/tmp/git/usr/lib64", bin_path: "/tmp/git/usr/bin"}}
 
   """
   def init do
-    base_dir = "/tmp/git"
-    with :ok = extract(base_dir),
-         :ok <- put_envs(base_dir) do
-      {:ok, base_dir}
+    if Application.get_env(:lambda_git, :enabled) do
+      with :ok = extract(),
+           :ok <- put_envs() do
+        {
+          :ok,
+          %{
+              template_dir: template_dir(),
+              exec_path: exec_path(),
+              ld_library_path: ld_library_path(),
+              bin_path: bin_path()
+          }
+        }
+      end
+    else
+      {:ok, nil}
     end
   end
 
-  def extract(base_dir) do
+  def extract do
     :code.priv_dir(:lambda_git)
     |> Path.join("git-2.4.3.tar")
-    |> :erl_tar.extract([{:cwd, base_dir}])
+    |> :erl_tar.extract([{:cwd, base_dir()}])
   end
 
-  def put_envs(base_dir) do
-    with :ok <- System.put_env("GIT_TEMPLATE_DIR", Path.join(base_dir, "usr/share/git-core/template")),
-         :ok <- System.put_env("GIT_EXEC_PATH", Path.join(base_dir, "usr/libexec/git-core")),
-         :ok <- System.put_env("LD_LIBRARY_PATH", Path.join(base_dir, "usr/lib64")),
-         :ok <- System.put_env("PATH", System.get_env("PATH") <>":" <> Path.join(base_dir, "usr/bin")) do
+  def put_envs do
+    with :ok <- System.put_env("GIT_TEMPLATE_DIR", template_dir()),
+         :ok <- System.put_env("GIT_EXEC_PATH", exec_path()),
+         :ok <- System.put_env("LD_LIBRARY_PATH", ld_library_path()),
+         :ok <- System.put_env("PATH", System.get_env("PATH") <>":" <> bin_path()) do
       :ok
     end
   end
+
+  def base_dir, do: "/tmp/git"
+  def template_dir, do: base_dir() |> Path.join("usr/share/git-core/template")
+  def exec_path, do: base_dir() |> Path.join("usr/libexec/git-core")
+  def ld_library_path, do: base_dir() |> Path.join("usr/lib64")
+  def bin_path, do: base_dir() |> Path.join("usr/bin")
 end
--- test/lambda_git_test.exs
+++ test/lambda_git_test.exs
@@ -3,10 +3,24 @@ defmodule LambdaGitTest do
   doctest LambdaGit
 
   test "intializes git" do
-    assert LambdaGit.init() == {:ok, "/tmp/git"}
-    assert System.get_env("GIT_TEMPLATE_DIR") == "/tmp/git/usr/share/git-core/template"
-    assert System.get_env("GIT_EXEC_PATH") == "/tmp/git/usr/libexec/git-core"
-    assert System.get_env("LD_LIBRARY_PATH") == "/tmp/git/usr/lib64"
-    assert System.get_env("PATH") =~ "/tmp/git/usr/bin"
+   template_dir = "/tmp/git/usr/share/git-core/template"
+   exec_path = "/tmp/git/usr/libexec/git-core"
+   ld_library_path = "/tmp/git/usr/lib64"
+   bin_path = "/tmp/git/usr/bin"
+
+    assert LambdaGit.init() == {
+      :ok,
+      %{
+        template_dir: template_dir,
+        exec_path: exec_path,
+        ld_library_path: ld_library_path,
+        bin_path: bin_path
+      }
+    }
+
+    assert System.get_env("GIT_TEMPLATE_DIR") == template_dir
+    assert System.get_env("GIT_EXEC_PATH") == exec_path
+    assert System.get_env("LD_LIBRARY_PATH") == ld_library_path
+    assert System.get_env("PATH") =~ bin_path
   end
 end
